26afbaa7ad57cab006757ab82c3917feec918d26
江小威 <jiangxiaowei@xbdedu.cn>
2016-03-28 23:22:07 +0800

    Initial Commit

---
+++ foodmenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  foodmenu
+//
+//  Created by 江小威 on 16/3/28.
+//  Copyright © 2016年 xavier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
